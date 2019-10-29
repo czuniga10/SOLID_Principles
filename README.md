5f1f6cfe964700dbbb8ca0590f8c253271bd015a
czuniga <chad.zuniga10@gmail.com>
2019-10-29 17:12:31 -0600

    Single Responsiblity Principle

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # SOLID_Principles
+______________________________________________________________
+
+# S - ## Single Responsibility Principle
+#### A class should only have one responsibility/ A class should only have one reason to change.
---
+++ S/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace S
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my application!");
+
+            //Ask for user information
+            Person user = new Person();
+
+            Console.Write("What is your first name: ");
+            user.FirstName = Console.ReadLine();
+
+            Console.Write("What is your last name: ");
+            user.LastName = Console.ReadLine();           
+            
+            //Checks to be sure the first and last names are valid
+            if (string.IsNullOrWhiteSpace(user.FirstName))
+            {
+                Console.WriteLine("You did not give us a valid first name!");
+                Console.ReadLine();
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(user.LastName))
+            {
+                Console.WriteLine("You did not give us a valid last name!");
+                Console.ReadLine();
+                return;
+            }
+        }
+    }
+}

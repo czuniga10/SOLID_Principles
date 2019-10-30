6bee177924f8327c802247f692f1108b4baf3a6a
czuniga <chad.zuniga10@gmail.com>
2019-10-30 14:32:26 -0600

    Single Responsibility Principle example finished.

---
+++ S/AccountGenerator.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S
+{
+    class AccountGenerator
+    {
+        public static void CreateAccount(Person user)
+        {
+            //Create a username for person
+            Console.WriteLine($"Your username is { user.FirstName.Substring(0,1) }{user.LastName}");
+        }
+    }
+}
--- S/Program.cs
+++ S/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace S
 {
@@ -6,31 +10,21 @@ namespace S
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to my application!");
+            StandardMessages.WelcomeMessage();
 
-            //Ask for user information
-            Person user = new Person();
+            Person user = PersonDataCapture.Capture();
 
-            Console.Write("What is your first name: ");
-            user.FirstName = Console.ReadLine();
+            bool isUserValid = PersonValidator.Validate(user);
 
-            Console.Write("What is your last name: ");
-            user.LastName = Console.ReadLine();           
+            if(isUserValid == false) 
+            {
+                StandardMessages.EndApplication();
+                return;
+            }
+
+            AccountGenerator.CreateAccount(user);
             
-            //Checks to be sure the first and last names are valid
-            if (string.IsNullOrWhiteSpace(user.FirstName))
-            {
-                Console.WriteLine("You did not give us a valid first name!");
-                Console.ReadLine();
-                return;
-            }
-
-            if (string.IsNullOrWhiteSpace(user.LastName))
-            {
-                Console.WriteLine("You did not give us a valid last name!");
-                Console.ReadLine();
-                return;
-            }
+            StandardMessages.EndApplication();
         }
     }
 }
